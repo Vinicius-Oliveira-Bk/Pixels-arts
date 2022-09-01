a36ad906300b26cf22c53cf83e934c17244d9e3a
Vinícius Byakko <37668297+Vinicius-Oliveira-Bk@users.noreply.github.com>
2022-09-01 19:04:23 -0300

    Adicionando requisito: 4

---
+++ script.js
@@ -0,0 +1,29 @@
+function generateColor() {
+  const exadecimais = '0123456789ABCDEF';
+  let color = '#';
+
+  for (let index = 0; index < 6; index += 1) {
+    color += exadecimais[Math.floor(Math.random() * 16)];
+  }
+  return document.getElementById("cor1").style.backgroundColor = color;
+}
+
+function generateColor1() {
+  const exadecimais = '123456789ABCDEF';
+  let color = '#';
+
+  for (let index = 0; index < 6; index += 1) {
+    color += exadecimais[Math.floor(Math.random() * 16)];
+  }
+  return document.getElementById("cor2").style.backgroundColor = color;
+}
+
+function generateColor2() {
+  const exadecimais = '23456789ABCDEF';
+  let color = '#';
+
+  for (let index = 0; index < 6; index += 1) {
+    color += exadecimais[Math.floor(Math.random() * 16)];
+  }
+  return document.getElementById("cor3").style.backgroundColor = color;
+}
